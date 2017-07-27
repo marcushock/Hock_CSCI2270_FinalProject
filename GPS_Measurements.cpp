f6b306e69ddbb698a6ad53f7c1fa4d2249c99951
Marcus Hock <marcus.hock@colorado.edu>
2017-07-26 19:29:09 -0600

    Tools to calculate the distance between GPS coordinates.

--- GPS_Measurements.cpp
+++ GPS_Measurements.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include "GPS_Measurements.h"
-
+#include <iostream>
+using namespace std;
 GPS_Measurements::GPS_Measurements()
 {
     //ctor
@@ -10,7 +12,13 @@ GPS_Measurements::~GPS_Measurements()
     //dtor
 }
 
-GPS_Measurements::CalculateDistance(double Lat1, double Long1, double Lat2, double Long2)
+double GPS_Measurements::CalculateDistance(double Lat1, double Long1, double Lat2, double Long2)
 {
-    return (Lat1+Lat2+Long1+Long2);
+    double rLat1 = Lat1/180.0*pi;
+    double rLong1 = Long1/180.0*pi;
+    double rLat2 = Lat2/180.0*pi;
+    double rLong2 = Long2/180.0*pi;
+    double a = sin((rLat1-rLat2)/2)*sin((rLat1-rLat2)/2)+cos(rLat1)*cos(rLat2)*sin((rLong1-rLong2)/2)*sin((rLong1-rLong2)/2);
+    double c = 2*atan2(sqrt(a),sqrt(1-a));
+    return c*r;
 }
--- GPS_Measurements.h
+++ GPS_Measurements.h
@@ -7,8 +7,11 @@ class GPS_Measurements
     public:
         GPS_Measurements();
         virtual ~GPS_Measurements();
+        double CalculateDistance(double a, double b, double c, double d);
     protected:
     private:
+        double pi = 3.14159265358979323846;
+        double r = 3959;
 };
 
 #endif // GPS_MEASUREMENTS_H
