42432ad6ea10d9a21962789aa2d7f62d43a41e07
Marcus Hock <marcus.hock@colorado.edu>
2017-07-26 21:30:54 -0600

    Current function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include "City_Nodes.h"
 
 using namespace std;
 
-
 int main(){
 	cout<<"Hello world"<<endl;
+	double a=1, b=2, c=3, d=4;
+	City_Nodes hello("cities.txt");
 return 0;
 }
