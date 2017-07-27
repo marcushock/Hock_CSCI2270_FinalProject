023d8f457a639b74a0f85650f5f69e0b417f5327
Marcus Hock <marcus.hock@colorado.edu>
2017-07-26 23:51:03 -0600

    simple menu added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include "City_Nodes.h"
 
 using namespace std;
 
+string menu(){
+	cout<<"■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"<<endl;
+	cout<<"Welcome"<<endl;
+	cout<<""<<endl;
+	cout<<""<<endl;
+	cout<<""<<endl;
+	string choice;
+	getline(cin,choice);
+	return choice;
+}
+
+string loadMenu(){
+	cout<<"Loading program..."<<endl;
+	cout<<"Please enter the name of the coordinate file."<<endl;
+	string fileName;
+	while (1){
+		getline(cin,fileName);
+		ifstream inFileStream;
+		string inLine;
+		inFileStream.open(fileName);
+		if (!inFileStream){cout<<"Could not read file.\nPlease enter the name correctly:\n(Maybe try 'cities.txt')\n";}
+		else{break;}
+	}
+	cout<<"Successfully loaded program"<<endl;
+	return fileName;
+}
+
 int main(){
-	cout<<"Hello world"<<endl;
-	double a=1, b=2, c=3, d=4;
-	City_Nodes hello("cities.txt");
+	City_Nodes runClass(loadMenu());
+	menu();
 return 0;
 }
